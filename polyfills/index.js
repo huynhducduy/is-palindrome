c2c53ea88402721dc24dd71d58fb06e7621d89dc
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2020-10-23 05:02:03 +0700

    add polyfills

---
+++ polyfills/index.js
@@ -0,0 +1,5 @@
+// replace(), toLowerCase(), TypeError, RangeError already have full support, no polyfill needed.
+require('./trim.js');
+require('./trimStart.js');
+require('./trimEnd.js');
+require('./normalize.js');
